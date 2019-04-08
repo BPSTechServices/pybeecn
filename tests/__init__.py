1fd862efc16fbacb9f9dd1ec7b66bd35a232508d
gabriel mcbride <gabriel.mcbride@twosixlabs.com>
2019-04-07 17:07:37 -0700

    Added some text to tests/__init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+# __init__ file required at test directory level for pytest to work
+# __init__ files can be placed at lower test directory levels however,
+# They are not necessary for pytest to run. 
